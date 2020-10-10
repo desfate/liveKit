f86c9ad4438e671cdb469db33af28d3c01aa30c0
yu jie <yujieworking@gmail.com>
2020-10-10 06:48:33 +0000

    LivePlayView commit

--- livekit/src/main/java/github/com/desfate/livekit/LivePlayView.java
+++ livekit/src/main/java/github/com/desfate/livekit/LivePlayView.java
@@ -2,13 +2,15 @@ package github.com.desfate.livekit;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
+import android.util.Size;
+import android.widget.RelativeLayout;
 
 import javax.microedition.khronos.egl.EGLConfig;
 
-import github.com.desfate.livekit.camera.CameraControl;
-import github.com.desfate.livekit.camera.view.FocusView;
+import github.com.desfate.livekit.camera.interfaces.CameraChangeCallback;
 import github.com.desfate.livekit.reders.CameraDrawer;
+import github.com.desfate.livekit.utils.JobExecutor;
+import github.com.desfate.livekit.utils.ScreenUtils;
 
 
 /**
