30edf92de3f0afaa4c032f5f8765e17245170ea8
yu jie <yujieworking@gmail.com>
2021-03-24 14:39:52 +0800

    yu.jie : 测试texture推流模式 推流角度问题

--- livekit/src/main/java/github/com/desfate/livekit/gl/draw/GLTexture2DFilter.java
+++ livekit/src/main/java/github/com/desfate/livekit/gl/draw/GLTexture2DFilter.java
@@ -34,7 +34,7 @@ public class GLTexture2DFilter {
                     "}\n";
 
     private static final FloatBuffer DEF_VERTEX_BUF = EglCore.createFloatBuffer(EglCore.CUBE);
-    private static final FloatBuffer DEF_TEX_BUF = EglCore.createFloatBuffer(EglCore.TEXTURE_ROTATION_0);
+    private static final FloatBuffer DEF_TEX_BUF = EglCore.createFloatBuffer(EglCore.TEXTURE_ROTATED_270);
     private static final FloatBuffer DEF_ROTATED_BUF = EglCore.createFloatBuffer(EglCore.TEXTURE_ROTATED_270);
 
     private int mProgramHandle;
